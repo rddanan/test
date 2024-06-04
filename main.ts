71f839ebeb55aa56e8a2f3a454646f4520d159cb
rddanan <139735850+rddanan@users.noreply.github.com>
2024-06-04 16:39:05 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    robot.motorSteer(45, 100)
+    basic.pause(1000)
+    robot.motorTank(-100, 100)
+    basic.pause(1000)
+    robot.motorSteer(-45, 100)
+    basic.pause(1000)
+    robot.motorSteer(45, 100)
+    basic.pause(500)
+    robot.motorSteer(-45, 100)
+    basic.pause(500)
+    robot.motorTank(100, 100)
+    basic.pause(5000)
+    robot.motorStop()
+})
 robot.elecfreaksCuteBot.start()
 basic.forever(function () {
 	
